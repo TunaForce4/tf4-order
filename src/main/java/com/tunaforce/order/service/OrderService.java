8e1c0f367f40bc78b8b90b26f1b96551f8580a73
legoChoi <chlx159@gmail.com>
2025-08-19 18:28:08 +0900

    :sparkles: feat: modify Order.java - add field `productId`

--- src/main/java/com/tunaforce/order/service/OrderService.java
+++ src/main/java/com/tunaforce/order/service/OrderService.java
@@ -39,20 +39,23 @@ public class OrderService {
     @Transactional
     public void createOrder(OrderCreateRequestDto request, UUID userId, UserRole role) {
         // 유저 역할에 따라 주문 생성에 요청된 업체와 로그인한 유저의 허브 또는 업체와의 관계가 유효한지 검증
+        // 허브 담당자일 경우 - 주문하려는 업체가 소속 업체인지 검증
         if (role.equals(UserRole.HUB)) {
             HubFindInfoResponseDto hubInfo = hubFeignClient.findHubInfoByUserId(userId);
             ProductFindInfoResponseDto productInfo = productFeignClient.findById(request.productId());
             validateUuidMatch(hubInfo.hubId(), productInfo.hubId());
         }
 
+        // 업체 담당자의 경우 - 주문하려는 업체가 담당 업체인지 확인
         if (role.equals(UserRole.COMPANY)) {
-            CompanyFindInfoResponseDto companyInfo = companyFeignClient.findHubInfoByUserId(userId);
+            CompanyFindInfoResponseDto companyInfo = companyFeignClient.findCompanyInfoByUserId(userId);
             ProductFindInfoResponseDto productInfo = productFeignClient.findById(request.productId());
             validateUuidMatch(companyInfo.companyId(), productInfo.companyId());
         }
 
+        // 배송 담당자의 경우 - 주문하려는 업체가 본인 소속 허브의 소속 업체인지 확인
         if (role.equals(UserRole.DELIVERY)) {
-            DeliveryFindInfoResponseDto deliveryInfo = deliveryFeignClient.findDeliveryInfoById(userId);
+            DeliveryFindInfoResponseDto deliveryInfo = deliveryFeignClient.findDeliveryInfoByUserId(userId);
             ProductFindInfoResponseDto productInfo = productFeignClient.findById(request.productId());
             validateUuidMatch(deliveryInfo.hubId(), productInfo.hubId());
         }
@@ -60,6 +63,7 @@ public class OrderService {
         // 주문 생성
         Order order = Order.builder()
                 .receiveCompanyId(request.receiveCompanyId())
+                .productId(request.productId())
                 .quantity(request.orderQuantity())
                 .requestMemo(request.requestMemo())
                 .status(OrderStatus.PENDING)
