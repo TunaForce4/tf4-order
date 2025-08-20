22f6a07acff9e51baf1df08ce6253d27cad6cf66
legoChoi <chlx159@gmail.com>
2025-08-20 15:19:24 +0900

    :bug: fix: modify authority validation for order creation

--- src/main/java/com/tunaforce/order/common/exception/OrderException.java
+++ src/main/java/com/tunaforce/order/common/exception/OrderException.java
@@ -13,6 +13,7 @@ public enum OrderException {
     UNSUFFICIENT_PRODUCT_QUANTITY(HttpStatus.CONFLICT, "상품 재고가 부족합니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     UNSUPPORTED_SORT_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 타입입니다."),
+    CANNOT_ORDER_OWN_PRODUCT(HttpStatus.BAD_REQUEST, "본인 허브 또는 업체의 상품을 주문할 수 없습니다."),
     PRODUCT_SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "서비스가 동작하지 않습니다.")
     ;
 
